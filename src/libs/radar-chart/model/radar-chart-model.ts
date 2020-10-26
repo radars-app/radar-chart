e15592c2488a0b9faef02570cac5a5eac49109f6
Uladzimir Svirydzenka <vladimirsviridenko1998@gmail.com>
2020-10-26 21:42:46 +0300

    fix: change build target to umd

---
+++ src/libs/radar-chart/model/radar-chart-model.ts
@@ -0,0 +1,5 @@
+export class RadarChartModel {
+	constructor() {
+		console.log('11')
+	}
+}
--- src/libs/radar-chart/radar-chart.ts
+++
@@ -1,6 +0,0 @@
-import './radar-chart.scss';
-
-export class RadarChart {
-	public title: string = 'radar-chart';
-	constructor() {}
-}
