7b1785676419c76c3e2ca84370f89c08b8d6500c
Uladzimir Svirydzenka <vladimirsviridenko1998@gmail.com>
2020-11-13 15:58:01 +0300

    refactor: remove demo starting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 export { RadarChartRenderer } from './libs/radar-chart/radar-chart.renderer';
 export { RadarChartModel } from './libs/radar-chart/radar-chart.model';
 export { RadarChartConfig } from './libs/radar-chart/radar-chart.config';
-
-if (process.env.NODE_ENV !== 'production') {
-	import ('./radar-demo')
-	.then((m: typeof import('./radar-demo')) => {
-		m.startDemo();
-	});
-}
