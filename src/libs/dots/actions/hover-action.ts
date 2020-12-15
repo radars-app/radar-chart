35a7bba13d98d1f8baa740a19910a7394cdac1de
Uladzimir Svirydzenka <57321079+vladimir-sviridenko@users.noreply.github.com>
2020-12-15 16:04:13 +0300

    refactor: remove extra variables in model

--- src/libs/dots/actions/hover-action.ts
+++ src/libs/dots/actions/hover-action.ts
@@ -1,4 +1,4 @@
-import { easeLinear } from 'd3';
+import { easeLinear, select, selectAll } from 'd3';
 import { Subject } from 'rxjs';
 import { DotAction } from '../../..';
 import { RadarDot } from '../../../models/radar-dot';
@@ -7,37 +7,26 @@ import { D3Selection } from '../../../models/types/d3-selection';
 export class HoverAction {
 	public static hoveredDotId: string = undefined;
 
-	public static applyTo(container: D3Selection, hoveredEmitter: Subject<DotAction>, hoveredOutEmitter: Subject<DotAction>): void {
+	public static applyTo(container: D3Selection, hoveredEmitter: Subject<DotAction>): void {
 		const hoveredClassName: string = 'dot--hovered';
+		const blurredClassName: string = 'dot--blurred';
 		const dot: RadarDot = container.datum();
 
 		container
 			.on('mouseenter', function (): void {
-				this.classList.add('dot--hovered');
 				HoverAction.hoveredDotId = dot.id;
 				hoveredEmitter.next({
 					dotId: dot.id,
 					selector: `.${hoveredClassName}`,
 				});
+				container.classed(hoveredClassName, HoverAction.hoveredDotId === dot.id);
+				selectAll('g.dot').classed(blurredClassName, true);
 			})
 			.on('mouseleave', function (): void {
+				selectAll('g.dot').classed(hoveredClassName, false);
+				selectAll('g.dot').classed(blurredClassName, false);
 				this.classList.remove(hoveredClassName);
 				HoverAction.hoveredDotId = undefined;
-				hoveredOutEmitter.next({
-					dotId: dot.id,
-				});
-			})
-			.transition()
-			.duration(200)
-			.ease(easeLinear)
-			.attr('fill-opacity', function (): number {
-				let opacity: number;
-				if (Boolean(HoverAction.hoveredDotId)) {
-					opacity = HoverAction.hoveredDotId === dot.id ? 1 : 0.6;
-				} else {
-					opacity = 0.8;
-				}
-				return opacity;
 			});
 	}
 }
